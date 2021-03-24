8f908de4eedf120dda7fb551d5dc3159744bf5ef
Mehedi Hasan <mhf221b@gmail.com>
2021-03-25 03:08:35 +0600

    Created a Registration form and a log in Form

--- OnlineCrimeReportingSystem/defaultAdmin/urls.py
+++ OnlineCrimeReportingSystem/defaultAdmin/urls.py
@@ -1,7 +1,12 @@
 from . import views
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
-    path('profile/', views.profile),
-    path('register/', views.register),
+    path('profile/', views.profile, name="profile"),
+    path('home/', views.home, name="home"),
+    path('register/', views.register, name="register"),
+    path('login/', auth_views.LoginView.as_view(template_name='defaultAdmin/login.html'), name ='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='defaultAdmin/logout.html'), name = 'logout'),
 ]
--- OnlineCrimeReportingSystem/defaultAdmin/views.py
+++ OnlineCrimeReportingSystem/defaultAdmin/views.py
@@ -1,24 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from .forms import AdminRegistrationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user, authenticate, login
 
 # Create your views here.
+@login_required
+def home(request):
+    return render(request, 'defaultAdmin/home.html')
+
+@login_required
 def profile(request):
+    user = get_user(request)
 
     contents = {
-        'name': "Mehedi Hasan",
-        'ID':"18201042",
-        'email': "mhf221b@gmail.com",
+        'name': user.username,
+        'email': user.email,
     }
     return render(request, 'defaultAdmin/profile.html', contents)
 
 
 def register(request):
     if request.method=='POST':
-        reg_form = UserCreationForm(request.POST)
+        reg_form = AdminRegistrationForm(request.POST)
         if reg_form.is_valid():
             reg_form.save()
+            username = reg_form.cleaned_data.get('username')
+            password = reg_form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
     else:
-        reg_form = UserCreationForm()
+        reg_form = AdminRegistrationForm()
 
     context = {
         'form': reg_form,
